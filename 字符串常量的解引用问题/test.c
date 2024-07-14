909866e4180544e389b7471a16e88fff4dfa32ac
klopp <2966755783@qq.com>
2024-07-14 11:20:14 +0800

    first_commit

---
+++ 字符串常量的解引用问题/test.c
@@ -0,0 +1,23 @@
+
+#include<stdio.h>
+
+
+void change_arr( const char* arr) //结论在不能改或不用改的变量尽量用 const修饰 (直接让编译器报语法错误)
+{
+	   // *arr = 0;//程序会挂掉
+		char* arr1 =*arr;//可以操作
+}
+
+
+int main()
+{
+
+	char *arr = "123456789";
+
+	//传进去的是一个字符串常量，*arr不可改相当 与 const char* arr ="123456789";
+	change_arr(arr);
+	printf("result = %s\n", arr);
+	printf("666");
+	return 0;
+}
+
